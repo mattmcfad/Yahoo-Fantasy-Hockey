7c4a797ef8fa9f57006548520d80c9726d8f68e5
Matt McFadyen <mmcfadyen91@gmail.com>
2015-02-07 20:02:30 -0500

    preparing to switch to express 3 for yahoosports

--- server.js
+++ server.js
@@ -15,13 +15,12 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/public/layouts"));
 
-
-
-
 app.get('/', function(req, res) {
 	res.render("index.jade", {dev: {name: "matt"}});
 });
 
+
 app.listen(port, function(){
-	util.log(util.colors.green("node server "), util.colors.magenta("localhost:"+port));
-})
+	util.log(util.colors.white("node server started: "),
+			util.colors.green("localhost:" + port));
+});
