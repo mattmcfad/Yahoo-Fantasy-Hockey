75b11eee34b8a97f055cdd823bb7e7e78d0026ca
Matt McFadyen <mmcfadyen91@gmail.com>
2015-02-10 23:27:55 -0500

    node oAuth baby steps

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,24 @@
 // keys.js in / dir contains yahoo consumerKey and consumerSecret
 var key = require("./keys"),
 	passport = require("passport"),
-	yahooAuth = require("passport-yahoo-oauth").Strategy;
+	YahooAuth = require("passport-yahoo-oauth").Strategy;
 
 var auth = {};
 
-
-auth.
+passport.use(
+	new YahooAuth({
+		consumerKey: key.consumerKey,
+		consumerSecret: key.consumerSecret,
+		callbackURL: "http://127.0.0.1:3000/auth/yahoo/callback"
+	},
+	function(token, tokenSecret, profile, done) {
+		User.findOrCreate(
+			{
+				yahooId: profile.id
+			},
+			function (err, user) {
+				return done(err, user);
+			}
+		);
+	}
+));
